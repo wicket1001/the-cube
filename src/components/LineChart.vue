2168d2e3c71832fa4f34fec487d683255ea55a14
Harald Moritz <harald.matthias.moritz@gmail.com>
2024-03-25 18:54:05 +0100

    Added LineChart.

---
+++ src/components/LineChart.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+import * as chartConfig from './chartConfig.js'
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+
+export default {
+  name: 'App',
+  components: {
+    Line
+  },
+  data() {
+    return chartConfig
+  }
+}
+
+</script>
+
+<template>
+  <Line :data="data" :options="options" />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import LineChart from '@/components/LineChart.vue'
+</script>
+
+<template>
+  <main>
+    <LineChart />
+  </main>
+</template>
