760de4c665bae7279863b81d2615ea1944ebde61
Harald Moritz <harald.matthias.moritz@gmail.com>
2024-04-22 22:29:43 +0200

    Physics have hidden values as base.

--- backend/Physics.py
+++ backend/Physics.py
@@ -2,30 +2,28 @@ from __future__ import annotations
 import math
 
 
-class Time(float):
-    def __new__(cls, value):
-        return float.__new__(cls, value)
+class Time:
+    value = 0
 
-    def __init__(self, value):
-        float.__init__(value)
+    def __init__(self, value: float):
+        self.value = value
 
     def __str__(self):
-        return f'{self:.2f}s'
+        return f'{self.value:.2f}s'
 
     @staticmethod
     def from_hours(time: float) -> Time:
         return Time(time * 3600)
 
 
-class Energy(float):
-    def __new__(cls, value):
-        return float.__new__(cls, value)
+class Energy:
+    value = 0
 
-    def __init__(self, value):
-        float.__init__(value)
+    def __init__(self, value: float):
+        self.value = value
 
     def __str__(self):
-        return f'{self:.2f}J'
+        return f'{self.value:.2f}J'
 
     @staticmethod
     def from_watt_seconds(energy: float) -> Energy:
@@ -40,46 +38,44 @@ class Energy(float):
         return Energy(energy * 3600 * 1000)
 
     def format_watt_seconds(self):
-        return f'{self:.2f}Ws'
+        return f'{self.value:.2f}Ws'
 
     def format_watt_hours(self):
-        return f'{self / 3600:.2f}Wh'
+        return f'{self.value / 3600:.2f}Wh'
 
     def format_kilo_watt_hours(self):
-        return f'{self / 3600 / 1000:.2f}kWh'
+        return f'{self.value / 3600 / 1000:.2f}kWh'
 
 
-class Power(float):
-    def __new__(cls, value):
-        return float.__new__(cls, value)
+class Power:
+    value = 0
 
-    def __init__(self, value):
-        float.__init__(value)
+    def __init__(self, value: float):
+        self.value = value
 
     def __str__(self):
-        return f'{self:.2f}W'
+        return f'{self.value:.2f}W'
 
     @staticmethod
     def from_kilo_watt(energy: float) -> Energy:
         return Energy(energy * 1000)
 
     def format_kilo_watt(self):
-        return f'{self / 1000:.2f}kW'
+        return f'{self.value / 1000:.2f}kW'
 
     def __mul__(self, other):
         if isinstance(other, Time):
-            return Energy(self * float(other))
+            return Energy(self.value * float(other.value))
 
 
-class Temperature(float):
-    def __new__(cls, value):
-        return float.__new__(cls, value)
+class Temperature:
+    value = 0
 
-    def __init__(self, value):
-        float.__init__(value)
+    def __init__(self, value: float):
+        self.value = value
 
     def __str__(self):
-        return f'{self:.2f}K'
+        return f'{self.value:.2f}K'
 
     @staticmethod
     def from_celsius(temperature: float) -> Temperature:
@@ -90,7 +86,7 @@ class Temperature(float):
         return Temperature((temperature - 459.67) * (5 / 9))
 
     def format_celsius(self):
-        return f'{self - 273.15:.2f}°C'
+        return f'{self.value - 273.15:.2f}°C'
 
     def format_fahrenheit(self):
-        return f'{self * (9 / 5) - 459.67:.2f}°F'
+        return f'{self.value * (9 / 5) - 459.67:.2f}°F'
