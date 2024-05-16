34c28eaa304444eb882ea7c04d3aaa31f4dba24c
Harald Moritz <harald.matthias.moritz@gmail.com>
2024-05-16 16:43:30 +0200

    Communication between arduino and python works now.

--- backend/arduino/serialReader/serialReader.ino
+++ backend/arduino/serialReader/serialReader.ino
@@ -1,12 +1,54 @@
-int x;
+String temp;
+String data;
+String name;
+String mini_str;
+String maxi_str;
+String value_str;
+float mini;
+float maxi;
+float value;
+float percent;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.setTimeout(1);
+  Serial.println("Ready");
+}
+
+float mapper(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void  loop() {
   while (!Serial.available());
-  x = Serial.readString().toInt();
-  Serial.print(x + 1);
+  temp = Serial.readString();
+  if (temp.indexOf("\n") == -1) {
+    data += temp;
+  } else {
+    data += temp;
+    data = data.substring(0, data.length() - 1);
+    // Serial.println(data);
+
+    name = data.substring(0, data.indexOf(";"));
+    data = data.substring(data.indexOf(";") + 1, data.length());
+
+    value_str = data.substring(0, data.indexOf(";"));
+    data = data.substring(data.indexOf(";") + 1, data.length());
+
+    mini_str = data.substring(0, data.indexOf(";"));
+    data = data.substring(data.indexOf(";") + 1, data.length());
+
+    maxi_str = data.substring(0, data.indexOf(";"));
+    data = data.substring(data.indexOf(";") + 1, data.length());
+
+    value = value_str.toFloat();
+    mini = mini_str.toFloat();
+    maxi = maxi_str.toFloat();
+
+    percent = mapper(value, mini, maxi, 0.0, 1.0);
+
+    Serial.println("Putting " + name + " speeding " + percent + " on " + value + " from " + mini + " to " + maxi);
+    
+    data = "";    
+  }
 }
--- backend/tests/test_arduino.py
+++ backend/tests/test_arduino.py
@@ -2,14 +2,30 @@
 import serial
 import time
 
-arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+arduino = serial.Serial(port='/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_75533353837351B0A150-if00', baudrate=9600, timeout=.1)  # /dev/ttyACM0
 
 
 def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
+    transfer = bytes(x, 'utf-8')
+    print(f"Sending \"{transfer}\"")
+    arduino.write(transfer)
     time.sleep(0.05)
-    data = arduino.readline()
-    return data
+    # data = arduino.readline()
+    return 'Send'
+
+
+def test_data_transfer():
+    # name;value;min;max
+    data = """temperature;15;-10;35
+storage;4000;0;13500
+radiation;0.125;0;1
+demand;350;0;1500
+wind;3.5;0;10
+generation;450;0;1500""".split('\n')
+    time.sleep(5)
+    for i in range(0, len(data)):
+        write_read(data[i] + '\n')
+        time.sleep(3)
 
 
 def test_arduino():
@@ -21,4 +37,4 @@ def test_arduino():
 
 if __name__ == '__main__':
     # https://projecthub.arduino.cc/ansh2919/serial-communication-between-python-and-arduino-663756
-    test_arduino()
+    test_data_transfer()
