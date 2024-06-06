a33afb662598fbdfd22cdf449bbce2e521fadf3b
Harald Moritz <harald.matthias.moritz@gmail.com>
2024-06-06 05:43:24 +0200

    Adding Griffins Turbine code.

---
+++ arduino/turbine/turbine.ino
@@ -0,0 +1,31 @@
+int sensorValue = 0;
+int outputValue = 1;
+bool newPeriod = true;
+unsigned long periodStart;
+unsigned long currentTime;  // running time for program
+int interval = `;
+
+void setup() {
+  pinMode(9, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  currentTime = millis();
+  if (newPeriod == true) {
+    periodStart = currentTime;
+    newPeriod = false;
+  }
+  if (currentTime - periodStart > interval) {
+    if (outputValue == 1) {
+      outputValue = 0;
+              digitalWrite(9, 0);
+
+    } else {
+      outputValue = 1;
+              digitalWrite(9, 1);
+    }
+          Serial.println(outputValue);
+    newPeriod = true;
+  }
+}
